9fb6a32b643b787f938d1ce6be74603071d0913d
Sam Gammon <sam@elide.ventures>
2023-05-26 22:40:54 -0700

    feat: add a dynamic name to the controller

--- src/main/kotlin/cloud/elide/sample/micronaut4/SampleController.kt
+++ src/main/kotlin/cloud/elide/sample/micronaut4/SampleController.kt
@@ -2,8 +2,9 @@ package cloud.elide.sample.micronaut4
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 /** Simple string response controller. */
 @Controller("/") class SampleController {
-    @Get fun hello(): String = "Hello, world!"
+    @Get fun hello(@QueryValue("name") name: String?): String = "Hello, ${name ?: "World"}!"
 }
